f4c7b7fe523eab58499f012588fec09ff79efa62
Rares Butean <buteanrares@gmail.com>
2022-10-11 19:48:30 +0300

    Last tasks

---
+++ Butean_Rares_Lab1/Controllers/CustomController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Butean_Rares_Lab1.Controllers
+{
+    public class CustomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome()
+        {
+            return "Welcome, user";
+        }
+
+        public string CustomWelcome(string username)
+        {
+            return $"Welcome, {username}";
+        }
+
+        public string Params(string param1, int param2)
+        {
+            return $"Your input was {param1}, {param2}";
+        }
+    }
+}
